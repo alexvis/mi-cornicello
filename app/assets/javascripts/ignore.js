b4e0770c84dcf5e362b7f6cdac30475c3b20aec2
Alex Visconde <andres.visconde@gmail.com>
2020-12-04 20:18:32 -0500

    google maps

--- app/assets/javascripts/ignore.js
+++ app/assets/javascripts/ignore.js
@@ -1,7 +1,6 @@
 var headEle = document.getElementsByTagName("head")[0]
 
-var key = "AIzaSyAVvMcEDDYQz2QU347ASQ1cSLlN81iLDn4"
-var mapKey = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAVvMcEDDYQz2QU347ASQ1cSLlN81iLDn4&callback=initMap&libraries=&v=weekly";
+var mapKey = "https://maps.googleapis.com/maps/api/js?key=&callback=initMap&libraries=&v=weekly";
 
 var script = document.createElement("script");
 script.setAttribute("src", mapKey);
