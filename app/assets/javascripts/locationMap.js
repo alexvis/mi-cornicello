e5316d38f9a355ef8f716c1f3409a27da946b63b
Alex Visconde <andres.visconde@gmail.com>
2020-12-04 20:20:33 -0500

    google map fix

--- app/assets/javascripts/locationMap.js
+++ app/assets/javascripts/locationMap.js
@@ -1,35 +1,35 @@
-"use strict";
-
-window.onload = initMap;
-
-let map;
-
-
-
-function initMap() {
-  // initial location variable
-  var location = {
-    center: { lat:-34.9011 , lng:-56.1645 },
-    zoom: 10,
-  }
-  // map object
-  map = new google.maps.Map(document.getElementById("map"), location)
-
-  // current location marker
-
-  var marker = new google.maps.Marker({
-    position: { lat:-34.9085 , lng:-56.1504 },
-    map: map,
-  })
-
-  // info window
-
-  var infoWindow = new google.maps.InfoWindow({
-    content: '<h1>Truck Location</h1>'
-  })
-
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-  })
-
-}
+// "use strict";
+//
+// window.onload = initMap;
+//
+// let map;
+//
+//
+//
+// function initMap() {
+//   // initial location variable
+//   var location = {
+//     center: { lat:-34.9011 , lng:-56.1645 },
+//     zoom: 10,
+//   }
+//   // map object
+//   map = new google.maps.Map(document.getElementById("map"), location)
+//
+//   // current location marker
+//
+//   var marker = new google.maps.Marker({
+//     position: { lat:-34.9085 , lng:-56.1504 },
+//     map: map,
+//   })
+//
+//   // info window
+//
+//   var infoWindow = new google.maps.InfoWindow({
+//     content: '<h1>Truck Location</h1>'
+//   })
+//
+//   marker.addListener('click', function() {
+//     infoWindow.open(map, marker);
+//   })
+//
+// }
