88a8d032f6e5fcb35d7b823e44abf8b76d4a9d06
Alex Visconde <andres.visconde@gmail.com>
2020-12-05 09:51:40 -0500

    credentials first commit

--- app/assets/javascripts/locationMap.js
+++ app/assets/javascripts/locationMap.js
@@ -1,35 +1,45 @@
-// "use strict";
-//
-// window.onload = initMap;
-//
-// let map;
-//
-//
-//
-// function initMap() {
-//   // initial location variable
-//   var location = {
-//     center: { lat:-34.9011 , lng:-56.1645 },
-//     zoom: 10,
-//   }
-//   // map object
-//   map = new google.maps.Map(document.getElementById("map"), location)
-//
-//   // current location marker
-//
-//   var marker = new google.maps.Marker({
-//     position: { lat:-34.9085 , lng:-56.1504 },
-//     map: map,
-//   })
-//
-//   // info window
-//
-//   var infoWindow = new google.maps.InfoWindow({
-//     content: '<h1>Truck Location</h1>'
-//   })
-//
-//   marker.addListener('click', function() {
-//     infoWindow.open(map, marker);
-//   })
-//
-// }
+"use strict";
+
+window.onload = initMap;
+
+let map;
+
+
+function initMap() {
+  var locationInfo = document.getElementById("location-info")
+  // latitude from user input
+  var lat = parseFloat(locationInfo["dataset"]["lat"])
+  // longitube from user input
+  var lng = parseFloat(locationInfo["dataset"]["lng"])
+  // initial location variable
+  var location = {
+    center: { lat:lat , lng:lng },
+    zoom: 10,
+  }
+  // map object
+  map = new google.maps.Map(document.getElementById("map"), location)
+
+  // current location marker
+
+  // var marker = new google.maps.Marker({
+  //   position: { lat:lat , lng:lng },
+  //   map: map,
+  // })
+  var input = document.getElementById('pac-input').value;
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  if (input ){
+
+    debugger
+  }
+  // info window
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<h1>Truck Location</h1>'
+  })
+
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  })
+
+}
--- app/controllers/truck_controller.rb
+++ app/controllers/truck_controller.rb
@@ -1,12 +1,18 @@
 class TruckController < ApplicationController
 
   def index
-    location = '22 main st Boston MA'
+    @loco = ""
+    location = '22 main st  c                                                                                                                                      MA'
     json = HTTP.get('https://maps.googleapis.com/maps/api/geocode/json',
       params:{
         address: location,
         key: 'AIzaSyAVvMcEDDYQz2QU347ASQ1cSLlN81iLDn4'
     })
     current_addres = JSON.parse(json)
+    @format_address = current_addres["results"][0]["formatted_address"]
+    @lat = current_addres["results"][0]["geometry"]["location"]["lat"]
+    @lng = current_addres["results"][0]["geometry"]["location"]["lng"]
+
   end
+
 end
