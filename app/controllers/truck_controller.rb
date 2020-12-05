9249449726523b038f910a9833b6ad563c66d32e
Alex Visconde <andres.visconde@gmail.com>
2020-12-04 19:58:30 -0500

    first google maps commit

--- app/controllers/truck_controller.rb
+++ app/controllers/truck_controller.rb
@@ -1,6 +1,12 @@
-class TruckController < ApplicationController 
+class TruckController < ApplicationController
 
   def index
-
+    location = '22 main st Boston MA'
+    json = HTTP.get('https://maps.googleapis.com/maps/api/geocode/json',
+      params:{
+        address: location,
+        key: 'AIzaSyAVvMcEDDYQz2QU347ASQ1cSLlN81iLDn4'
+    })
+    current_addres = JSON.parse(json)
   end
 end
